bc90b463d3eea8b50ce4f3ba294a7a2195f65c49
Hamish Haggerty <hamish.haggerty@uqconnect.edu.au>
2022-07-14 15:48:12 +1000

    Edited setup.py to add dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
 name='Base_Stein',
 packages=find_packages(where='src'),
 package_dir={'': 'src'},
-url='https://github.com/hamish-haggerty/AI-hacking/tree/master/SVGD/SVGD_packages' #comment this out /delete if it doesn't work...
+    install_requires=["torch>=1.11.0",
+                      "pytest>=7.1.2"]
 )
